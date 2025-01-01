d2ff1792778d0633e6861a6e227149a25b324486
angoloisaac83 <angoloisaac83@gmail.com>
2025-01-01 08:55:50 -0800

    first commit

---
+++ api/server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Path to JSON file
+const filePath = '../data.json';
+
+// Read JSON file
+const getData = () => {
+  const data = fs.readFileSync(filePath);
+  return JSON.parse(data);
+};
+
+// Write JSON file
+const saveData = (data) => {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+};
+
+// GET all drinks from a specific category
+app.get('/api/drinks', (req, res) => {
+  const { category } = req.query;
+  const data = getData();
+  if (category && data[category]) {
+    res.json(data[category]);
+  } else {
+    res.status(400).json({ message: 'Category not found!' });
+  }
+});
+
+// POST add a new drink to a specific category
+app.post('/api/drinks', (req, res) => {
+  const { category, drink } = req.body;
+  const data = getData();
+
+  if (data[category]) {
+    data[category].push(drink);
+    saveData(data);
+    res.json(drink);
+  } else {
+    res.status(400).json({ message: 'Category not found!' });
+  }
+});
+
+// PUT edit a drink (using name)
+app.put('/api/drinks/:category/:name', (req, res) => {
+  const { category, name } = req.params;
+  const updatedDrink = req.body;
+  const data = getData();
+
+  const drinkIndex = data[category].findIndex((drink) => drink.name === name);
+  if (drinkIndex !== -1) {
+    data[category][drinkIndex] = { ...data[category][drinkIndex], ...updatedDrink };
+    saveData(data);
+    res.json(data[category][drinkIndex]);
+  } else {
+    res.status(404).json({ message: 'Drink not found!' });
+  }
+});
+
+// DELETE a drink (using name)
+app.delete('/api/drinks/:category/:name', (req, res) => {
+  const { category, name } = req.params;
+  const data = getData();
+
+  const drinkIndex = data[category].findIndex((drink) => drink.name === name);
+  if (drinkIndex !== -1) {
+    data[category].splice(drinkIndex, 1);
+    saveData(data);
+    res.json({ message: 'Drink deleted successfully!' });
+  } else {
+    res.status(404).json({ message: 'Drink not found!' });
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
